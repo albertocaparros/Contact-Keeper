005d0a7725667b95146ab03c70704128cc14b891
albertocaparros <acaparros87@gmail.com>
2019-07-02 10:16:25 +0200

    Martes, en mitad de auth

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 //Connect database 
 connectDB();
 
+// Init Middleware
+
+app.use(express.json({ extended: false}));
+
 app.get('/', (req,res) => res.json({msg: "Welcome to the contact keeper API"}));
 
 
